254cc3e47c4370c7a24357390bc38907b8fa2352
TakGlobus <tkurihana@uchicago.edu>
2019-12-02 16:30:16 -0600

    11/29-11/30 Tak experiment scripts

--- tf-benchmark/distributed.py
+++ tf-benchmark/distributed.py
@@ -1,60 +1,71 @@
+
 import os
 import time
 import argparse
 import numpy as np
 import tensorflow as tf
+import tensorflow_datasets as tfds
 
 from tensorflow.python.keras.layers import *                                                    
 from tensorflow.python.keras.models import Model
 from tensorflow.examples.tutorials.mnist import input_data
 from tensorflow.python.client import timeline
+#from tensorflow.keras.backend import set_session
 
 # Hyper-parameters
-learning_rate = 0.01
-epoch = 10
-nbatch = 32
+learning_rate = 0.001
+iteration = 10
+#iteration = 100
+#nbatch = args.batch_size
 
 # image shapes
-height = 28
-width  = 28
+#height = args.height
+#width  = args.width
+#height = 32
+#width  = 32
+#height = 224
+#width  = 224
+channel=1
+
+# image label class
+num_classes = 10
 
 def main(args):
+
+    # args 
+    nbatch = args.batch_size
+    height = args.height
+    width  = args.width
+
     # Select model
     #   mlp or mobilenet or resnet
     if args.mtype == 'mlp': 
-      from mlp import model_class
+      from mlp import model_fn
     elif args.mtype == 'mobilenet': 
-      from mobilenet import mobilenet as model_class
+      from mobilenet import model_fn
     elif args.mtype == 'resnet': 
-      from resnet import resnet as model_class
-
-    num_node = args.numnode
-    worker_nodes = []
-
-    if num_node == 2:
-        worker_nodes = ['128.135.24.250:2223']
-    elif args.numnode == 3:
-        worker_nodes = ['128.135.24.250:2223', '128.135.24.252:2223']
-    elif args.numnode == 4:
-        worker_nodes = ['128.135.24.250:2223', '128.135.24.252:2223', '128.135.24.253:2223']
-    
-    # get data
-    mnist = input_data.read_data_sets(os.path.abspath("../dataset/mnist/"), one_hot=True)
-    train_images =  mnist.train.images
-    train_labels =  mnist.train.labels
-
-    # build data pipeline 
-    dataset = input_fn(train_images, train_labels, mtype=args.mtype)
-    train_iterator = dataset.make_initializable_iterator()
-    imgs, labels = train_iterator.get_next()
+      from resnet import model_fn
 
     # Attempting to connect all nodes in `tf.train.ClusterSpec`.
+    # Specify your machine's IP and Port
     cluster_spec = tf.train.ClusterSpec({
-        'worker': worker_nodes,
-        'ps': ['128.135.24.251:2222'],
+        'worker': [
+            '10.143.3.3:2222'
+        ],
+        'ps': ['128.135.164.173:55555'],
     })
 
-    server = tf.train.Server(cluster_spec, job_name=args.job_name, task_index=args.task_index)
+    # Specify GPU config here otherwise distributed training won't reflect the config
+    config = tf.ConfigProto(
+              gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction = 0.6),
+              allow_soft_placement=True,
+    )
+
+    # Add config option specified above. O.w. PS & Worker server may consume all GPU memory
+    server = tf.train.Server(cluster_spec,
+                             job_name=args.job_name,
+                             task_index=args.task_index,
+                             config=config)
 
     if args.job_name == "ps":
         # `server.join()` means it's NEVER killed
@@ -63,41 +74,51 @@ def main(args):
         # Store the variables into `ps`.
         print("\n   Worker Wake up!   \n", flush=True)
         with tf.device(tf.train.replica_device_setter(
+                worker_device="/job:worker/task:%d" % args.task_index,
                 cluster=cluster_spec)):
 
+            # get data
+            ### MNIST
+            #mnist = input_data.read_data_sets(os.path.abspath("./MNIST_data/"), one_hot=True)
+            #train_images =  mnist.test.images
+            #train_labels =  mnist.test.labels
+            #dataset = input_fn(train_images, train_labels,height, width, nbatch, args.mtype)
+
+            ### Imagenet2012
+            datasets = tfds.load('imagenet2012', data_dir=)
+            dataset  = input_imgnet_fn(datasets, args.nbatch) 
+            train_iterator = dataset.make_initializable_iterator()
+            imgs, labels = train_iterator.get_next()
+
+            # global step config
             global_step = tf.train.create_global_step()
             get_global_step = tf.train.get_global_step()
 
-            # Model
-            try:
-              if args.mtype == 'mlp' :
-                model = model_class(net_name='mlp')
-              elif args.mtype ==  'mobilenet':
-                model = model_class(net_name='mobilenet', model_layer=17, input_h=height, input_w=width, num_classes=10)
-              elif args.mtype ==  'resnet':
-                model = model_class(net_name='resnet', model_layer=50, input_h=height, input_w=width, num_classes=10)
-            except:
-              raise NameError("\n ###  Error! Make sure your model type argument is mlp or mobilenet or resnet ###\n")
+            # Model functional
+            if args.mtype == 'mlp':
+              model = model_fn(input_shape=(height*width,), num_classes=num_classes)
+            else :
+              model = model_fn(input_shape=(height, width, channel), num_classes=num_classes)
 
-            # mlp
-            preds = model.build(imgs)
+            # prediction
+            preds = model(imgs)
 
-            # Metrics
-            loss = tf.reduce_mean(tf.losses.softmax_cross_entropy(onehot_labels=labels, logits=preds))
+            # training options
+            loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=preds))
             optimizer = tf.train.AdamOptimizer(learning_rate)
             train_op = optimizer.minimize(loss, global_step=global_step)
 
-            is_chief = (args.task_index == 0)
-
-            hooks = [tf.train.StopAtStepHook(last_step=epoch),
-                     tf.train.CheckpointSaverHook('./example-save',save_steps=epoch,
-                                                  saver=tf.train.Saver(max_to_keep=1))]
 
             # Initialize the variables, if `is_chief`.
-            config = tf.ConfigProto(
-                    allow_soft_placement=True,
-                    device_filters=['/job:ps', '/job:worker/task:%d' % args.task_index],
-            )
+            is_chief = (args.task_index == 0)
+
+            # Terminate hook
+            hooks = [tf.train.StopAtStepHook(last_step=iteration),
+                     tf.train.CheckpointSaverHook('./example-save',
+                                                  save_steps=iteration,
+                                                  saver=tf.train.Saver(max_to_keep=1))]
+            
+            # Training
             with tf.train.MonitoredTrainingSession(
                     is_chief=is_chief,
                     config=config,
@@ -106,44 +127,57 @@ def main(args):
 
                 sess.run(train_iterator.initializer)
                 start_time = time.perf_counter()
-                start_clock = time.clock()
                 while not sess.should_stop():
                     step, _, train_loss = sess.run([get_global_step, train_op, loss])
-                    print('In {step} step: loss = {loss}'.format(step=step, loss=train_loss))
+                    print('In {step} step: loss = {loss}'
+                          .format(step=step, loss=train_loss))
                 stop_time = time.perf_counter()
-                stop_clock = time.clock()
+            stop_time = time.perf_counter()
         
-                print('Duration (via time.perf_counter()): %f (%f - %f)' % (stop_time - start_time, stop_time, start_time))
-                print('Clock (via time.clock()): %f (%f - %f)' % (stop_clock - start_clock, stop_clock, start_clock))
+            # logout
+            print('  duration (via time.perf_counter()): %f (%f - %f)' % (stop_time - start_time, stop_time, start_time))
+            print('  Throughput [image/sec] : %f  ' % (nbatch*iteration/(stop_time - start_time) ) )
+            
+            # save log
+            os.makedirs("./log/MNIST", exist_ok=True)
+            with open("./log/MNIST/"+args.expname+'.txt', "w") as f:
+              f.write(str(stop_time - start_time)+','+str(nbatch*iteration/(stop_time - start_time)))
 
-        # It's able to fetch variables in another session.
-        #if is_chief:
-        #    with tf.Session(server.target) as sess:
-        #        w, b, acc = sess.run([w, b, accuracy],
-        #                             feed_dict={x: X_test, y: y_test})
-        #        print('accuracy in validation = ', acc)
-        #        print('bias = ', b)
-        #        print('weight = ', w)
+def input_imgnet_fn(dataset, nbatch=32):
+    dataset = dataset.batch(nbatch)
+    return dataset
 
-def input_fn(images, labels, height=32, width=32, mtype='mlp',prefetch=1):
-  """ 
-    INPUT:  imagess : MNIST image [batch, height, width, channel]. np.ndarray
-            labels  : MNIST image labels.  This should be one-hot 
+
+def input_fn(images, labels, height=32, width=32, nbatch=32, model_type='mlp',prefetch=1):
+    """ 
+    INPUT:  imagess : numpy image [batch, height, width, channel]. np.ndarray
+            labels  : numpy image labels.  This should be one-hot 
             height/width : resized height, width
-            mtype   : model type
+            model_type   : model type
             nbatch : batch size
 
     OUTPUT: tf.data object
-  """
-  # reshape other than mlp model
-  images = images.reshape(-1,28,28,1)
-  images = tf.image.resize(images, (height, width))
-  if mtype == 'mlp':
-    images = tf.reshape(images, [-1,height*width])
+    """
+    # reshape other than mlp model
+    ndim = len(images.shape)
+    print(ndim, flush=True)
+    if ndim <= 3:
+      # case MNIST, images = (batch, h x w, c)
+      if ndim == 2:
+        _, hw = np.shape(images)
+        c = 1
+      elif ndim == 3:
+        _, hw, c = np.shape(images)
+      h = w = int(np.sqrt(hw))
+      images = images.reshape(-1,h,w,c)
+
+    images = tf.image.resize(images, (height, width))
+    if model_type == 'mlp':
+      images = tf.reshape(images, [-1,height*width])
   
-  dataset = tf.data.Dataset.from_tensor_slices((images, labels))
-  dataset = dataset.shuffle(500).repeat().batch(nbatch).prefetch(prefetch)
-  return dataset
+    dataset = tf.data.Dataset.from_tensor_slices((images, labels))
+    dataset = dataset.batch(nbatch)
+    return dataset
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -153,5 +187,12 @@ if __name__ == '__main__':
                         default=0)
     parser.add_argument('--mtype', dest='mtype', type=str,
                         default='mlp')
-    parser.add_argument('--node', dest='numnode', action='store', type=int, default=2)
+    parser.add_argument('--expname', dest='expname', type=str,
+                        default='default')
+    parser.add_argument('--batch_size', dest='batch_size', type=int,
+                        default=32)
+    parser.add_argument('--height', dest='height', type=int,
+                        default=28)
+    parser.add_argument('--width', dest='width', type=int,
+                        default=28)
     main(parser.parse_args())
