fb73fe91d6f386b605972d3f33992fb66951b194
TakGlobus <tkurihana@uchicago.edu>
2019-12-07 14:58:20 -0600

    12/07 update exp script

--- tf-benchmark/job.bash
+++ tf-benchmark/job.bash
@@ -7,52 +7,86 @@
 #
 
 ## Config
-job_name="worker"
+job_name='worker'
 task_index=0
-height=28
-width=28
-# Mobilenet
+ps_ip_port=("128.135.164.173:22222")
+worker_ip_port=("10.143.3.3:22222" "10.150.49.25:22222")
+network_name="enp4s0" # your network name
+
+## python path
+# * MUST specify since we run by root
+#python_path="python"
+python_path="/home/tkurihana/.conda/envs/tf-gpu/bin/python"
+
+## Dataset
+dataname='mnist' # or imagenet
+datadir='./MNIST' 
+
+## Height/Width
+height=32
+width=32
+# Mobilenet will accept 32 or larger Height/Widht
 #height=32
 #width=32
 
 ## Lists
 #models=("mlp" "mobilenet" "resnet")
 models=("mlp")
-batch_sizes=(32 128 1024)
+batch_sizes=(32)
+#batch_sizes=(32 128 1024)
 
 ## EXP config list
-delays=(1 10 100)  #ms
-bandwidths=(1000 100 10)  #Mbits
-packets=(0 1 10) # percent loss
+delays=(0.1)  #ms
+bandwidths=(1000)  #Mbits
+packets=(0) # percent loss
+#delays=(1 10 100)  #ms
+#bandwidths=(1000 100 10)  #Mbits
+#packets=(0 1 10) # percent loss
 
 echo 'START PERFORMANCE EXP'
 
-# initial settings
-echo `sudo tc qdisc add dev enp4s0 root netem delay 1ms`
-sleep 10
+if  [ ${job_name} = "ps" ]; then
+  echo "PS"
+  $python_path distributed.py \
+        --job-name ${job_name} --task-index ${task_index} --mtype "mlp" \
+        --ps-ip-port ${ps_ip_port} \
+        --worker-ip-port ${worker_ip_port} \
+        --dataname ${dataname} \
+        --datadir ${datadir} \
+        --expname "param-server"  --batch_size 1 --height 1 --width 1
+fi
+if  [ ${job_name} = "worker" ]; then
+  echo "WORKER"
+  # initial settings
+  echo `sudo tc qdisc add dev ${network_name} root netem delay 1ms`
+  sleep 10
 
-for model in "${models[@]}" ; do
-  for delay in "${delays[@]}" ; do
-    for bandwidth in "${bandwidths[@]}" ; do
-      for packet in "${packets[@]}" ; do
-        for batch_size in "${batch_sizes[@]}" ; do
-          # START
-          cexpname="${model}_${batch_size}-${packet}ps-${bandwidth}mbit-${delay}ms"
+  for model in "${models[@]}" ; do
+    for delay in "${delays[@]}" ; do
+      for bandwidth in "${bandwidths[@]}" ; do
+        for packet in "${packets[@]}" ; do
+          for batch_size in "${batch_sizes[@]}" ; do
+            # START
+            cexpname="${model}_${batch_size}-${packet}ps-${bandwidth}mbit-${delay}ms"
 
-          echo `sudo tc qdisc change dev enp4s0 root netem delay ${delay} loss ${packet}% rate ${bandwidth}mbit `
+            echo `sudo tc qdisc change dev ${network_name} root netem delay ${delay} loss ${packet}% rate ${bandwidth}mbit `
 
-          /home/tkurihana/.conda/envs/tf-gpu/bin/python distributed.py \
-                --job-name=${job_name} --task-index=${task_index} --mtype=${model} \
-                --expname=${cexpname}  --batch_size=${batch_size} --height=${height} --width=${width} 
-          # END
-          sleep 5
+            $python_path distributed.py \
+                  --job-name ${job_name} --task-index ${task_index} --mtype ${model} \
+                  --ps-ip-port ${ps_ip_port} \
+                  --worker-ip-port ${worker_ip_port} \
+                  --dataname ${dataname} \
+                  --datadir ${datadir} \
+                  --expname ${cexpname}  --batch_size ${batch_size} --height ${height} --width ${width} 
+            # END
+            sleep 5
+          done
         done
       done
     done
   done
-done
+  echo `sudo tc qdisc del dev ${network_name} root`
+fi
 
-
-echo `sudo tc qdisc del dev enp4s0 root`
 echo 'NORMAL END'
 
